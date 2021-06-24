e58a75f389e28ecf017dfa92927f130473dc25c5
anthonywaight <anthony.waight1@gmail.com>
2021-06-23 21:52:49 -0500

    registered models in Django admin app

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import User, Bid, Listing, Comments
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Bid)
+admin.site.register(Listing)
+admin.site.register(Comments)
