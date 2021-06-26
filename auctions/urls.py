937ff5e72b17d52be43a2353dafd9a0197dd7837
awaigh <76921309+awaigh@users.noreply.github.com>
2021-06-26 16:51:16 -0500

    added a new view listing page

--- auctions/urls.py
+++ auctions/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    path("createlisting", views.createListing, name="createListing")
+    path("createlisting", views.createListing, name="createListing"),
+    path("checkListing<str:product_id>", views.checkListing, name="checkListing"),
+    path("activeListing", views.activeListing, name="activeListing")
 ]
